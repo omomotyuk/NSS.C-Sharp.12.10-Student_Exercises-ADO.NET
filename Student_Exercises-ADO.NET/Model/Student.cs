da1d8e388e5fbbb1eaeb6e3fda61be794e4567e4
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-12-10 14:33:43 -0600

    added model classes

---
+++ Student_Exercises-ADO.NET/Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student_Exercises_ADO.NET.Model
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SlackHandle { get; set; }
+        public int CohortId { get; set; }
+    }
+}
