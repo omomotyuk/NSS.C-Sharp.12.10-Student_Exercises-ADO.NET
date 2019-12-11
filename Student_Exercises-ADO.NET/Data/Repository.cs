b34cbb7904c2de5a79c20c3855db3be84d899fb5
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-12-10 22:36:09 -0600

    added 1 action: Query the database for all the Exercises

--- Student_Exercises-ADO.NET/Data/Repository.cs
+++ Student_Exercises-ADO.NET/Data/Repository.cs
@@ -1,10 +1,86 @@
-﻿using System;
+﻿using Student_Exercises_ADO.NET.Interface;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Reflection;
 using System.Text;
 
 namespace Student_Exercises_ADO.NET.Data
 {
-    class Repository
+    class Repository<T> where T : IRecord, new()
     {
+        /// <summary>
+        ///  Represents a connection to the database.
+        ///   This is a "tunnel" to connect the application to the database.
+        ///   All communication between the application and database passes through this connection.
+        /// </summary>
+        public SqlConnection Connection
+        {
+            get
+            {
+                string _connectionString = "Data Source=localhost\\SQLEXPRESS;" +
+                    "Initial Catalog=StudentExercises;" +
+                    "Integrated Security=True";
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+        /// <summary>
+        ///  Returns a list of all selected fields in the database
+        /// </summary>
+        public List<T> GetAll( string table, List<string> fields )
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = GetSelectQuery(table, fields);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    /*
+                    Dictionary<string, int> ordinalDict = new Dictionary<string, int>();
+                    foreach( string field in fields )
+                    {
+                        ordinalDict.Add( field, reader.GetOrdinal( field ) );
+                    }
+                    */
+                    List<T> itemList = new List<T>();
+
+                    Dictionary<string, string> fieldValues = new Dictionary<string, string>();
+                    while (reader.Read())
+                    {
+                        foreach( string field in fields )
+                        {
+                            int position = reader.GetOrdinal(field);
+                            string value = "";
+                            try
+                            {
+                                value = reader.GetString(position);
+                            } catch
+                            {
+                                value = reader.GetInt32(position).ToString();
+                            }
+                            fieldValues.Add( field, value );
+                        }
+
+                        var newItem = new T();
+                        newItem.Set( fieldValues );
+                        itemList.Add( newItem );
+
+                        fieldValues.Clear();
+                    }
+
+                    reader.Close();
+
+                    return itemList;
+                }
+            }
+        }
+
+        public string GetSelectQuery( string table, List<string> fields )
+        {
+            return String.Concat("SELECT ", String.Join(", ", fields), " FROM ", table);
+        }
     }
 }
--- Student_Exercises-ADO.NET/Program.cs
+++ Student_Exercises-ADO.NET/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Student_Exercises_ADO.NET.Data;
+using Student_Exercises_ADO.NET.Model;
+using System;
+using System.Collections.Generic;
 
 namespace Student_Exercises_ADO.NET
 {
@@ -6,15 +9,44 @@ namespace Student_Exercises_ADO.NET
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            /*
-                Query the database for all the Exercises.
-                Find all the exercises in the database where the language is JavaScript.
-                Insert a new exercise into the database.
-                Find all instructors in the database. Include each instructor's cohort.
-                Insert a new instructor into the database. Assign the instructor to an existing cohort.
-                Assign an existing exercise to an existing student.
-            */
+            // Query the database for all the Exercises.
+            Action_AllExercises();
+
+            // Find all the exercises in the database where the language is JavaScript.
+            // Insert a new exercise into the database.
+            // Find all instructors in the database. Include each instructor's cohort.
+            // Insert a new instructor into the database. Assign the instructor to an existing cohort.
+            // Assign an existing exercise to an existing student.
+
+            //Action_AllStudent();
+        }
+
+        static void Action_AllExercises()
+        {
+            var exerciseRepository = new Repository<Exercise>();
+            List<string> exerciseFields = new List<string>() { "Name", "Language" };
+            var allExercises = exerciseRepository.GetAll("Exercise", exerciseFields);
+            Console.WriteLine("\nAll Exercises:\n" +
+                                "--------------");
+            foreach (var exercise in allExercises)
+            {
+                Console.WriteLine($"\"{exercise.Name}\" ({exercise.Language})");
+            }
+            Console.Write("\n");
+        }
+
+        static void Action_AllStudent()
+        {
+            var repository = new Repository<Student>();
+            List<string> fields = new List<string>() { "FirstName", "LastName", "CohortId" };
+            var allStudents = repository.GetAll("Student", fields);
+            Console.WriteLine("\nAll Students:\n" +
+                                "-------------");
+            foreach (var student in allStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is from cohort {student.CohortId}");
+            }
+            Console.Write("\n");
         }
     }
 }
